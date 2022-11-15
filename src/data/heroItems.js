2b30f48c03e9b6a0736250f307554c6fce542961
Bekithemba-Mdluli <putthetrust.gamedev@gmail.com>
2022-11-15 18:07:55 +0200

    updated titles in hero

--- src/data/heroItems.js
+++ src/data/heroItems.js
@@ -5,22 +5,22 @@
 
 const heroItems = [
   {
-    title: "Some Construction Title",
+    title: "Some Construction Title 1",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus.",
     image: require("../images/services/cons.jpg"),
   },
   {
-    title: "Some Construction Title",
+    title: "Some Construction Title 2",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus.",
     image: require("../images/hero/painting.jpg"),
   },
   {
-    title: "Some Construction Title",
+    title: "Some Construction Title 3",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus.",
     image: require("../images/hero/foundation.jpg"),
   },
   {
-    title: "Some Construction Title",
+    title: "Some Construction Title 4",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus.",
     image: require("../images/hero/building.jpg"),
   },
