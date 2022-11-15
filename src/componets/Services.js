f28aaf57a35798add7fb42a4831b5a9cff82464d
Bekithemba-Mdluli <putthetrust.gamedev@gmail.com>
2022-11-15 17:54:47 +0200

    Fixed Width

--- src/componets/Services.js
+++ src/componets/Services.js
@@ -1,17 +1,10 @@
 import React from "react";
-import shin from "../images/services/shinra.png";
-import paint from "../images/services/paint.png";
+
+import services from "../data/serviceItems";
 
 import "./Services.css";
 
 function Services() {
-  const services = [
-    { name: "Bricklaying", img: paint },
-    { name: "Capentry", img: shin },
-    { name: "Electrical", img: shin },
-    { name: "Waterproofing", img: shin },
-  ];
-
   return (
     <div className="services">
       <h2>Our Unlimited Services</h2>
---
+++ src/data/serviceItems.js
@@ -0,0 +1,8 @@
+const services = [
+  { name: "Bricklaying", img: require("../images/services/paint.png") },
+  { name: "Capentry", img: require("../images/services/shinra.png") },
+  { name: "Electrical", img: require("../images/services/shinra.png") },
+  { name: "Waterproofing", img: require("../images/services/paint.png") },
+];
+
+export default services;
