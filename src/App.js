92496aa5ba06d026eea4b8b465fe94967f948bd1
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2022-11-16 15:02:24 +0200

    New Gift Branch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import "./App.css";
-import AboutUs from "./componets/AboutUs";
-import Hero from "./componets/Hero";
-import Services from "./componets/Services";
+import Navbar from './components/Navbar';
+import './App.css';
+import logo from './components/Images/logo.png'
 
 function App() {
   return (
-    <div>
-      <Hero />
-      <Services />
-      <AboutUs />
+    <div className="App">
+        <Navbar />
+        <img src={logo} alt="smaple" />
+        <img src={logo} alt="smaple" />
+        <img src={logo} alt="smaple" />
+        <p>ef;isjfdjf;dj;dja;sj;sadj;sja;sddas</p>
+        <p>lorem ipusmsldjfnldjncsljdfldsnfdsff</p>
+        <p>sdfdfdsfddddddddfsfsdfd</p>
+        <p>sdfsdffsfdfdfdfdfdfdfsfd</p>
+        <p>dsfsdkjfhfkjhfskjfhksdjfdsdf</p>
+        <p>dsfdfdsfldfjdfjodsjidofsjojijodsf</p>
+        <p>sdfdsfhodhfsoudhfouhfoshdfdf</p>
+        <p>dsfdofhsoudhfoudshohsdoufhodhfdsf</p>
+        <p>dsfdsofihsfihfoushfdouhfiudhfds</p>
+        <p>dsfpdsjidofjsijfsjduhcuhiushdciubcdsc</p>
+        <p>sdfeisjfoisfejhosudhcudhchuhcshfouheoshfdc</p>
+        <p>sepspidhsocubsoufbuef fsudhfduhsohfdf</p>
+        <p>sdfpejs soeifodihfdouhsd fdsoufdf</p>
+        <p>sfesifhosuhf sdoufhofuhsoudhfodusfd</p>
+        <p>sdffijpsdofdsonfununfoenfunou</p>
+        <p>sdfdsfhodhfsoudhfouhfoshdfdf</p>
+        <p>dsfdofhsoudhfoudshohsdoufhodhfdsf</p>
+        <p>dsfdsofihsfihfoushfdouhfiudhfds</p>
+        <p>dsfpdsjidofjsijfsjduhcuhiushdciubcdsc</p>
+        <p>sdfeisjfoisfejhosudhcudhchuhcshfouheoshfdc</p>
+        <p>sepspidhsocubsoufbuef fsudhfduhsohfdf</p>
+        <p>sdfpejs soeifodihfdouhsd fdsoufdf</p>
+        <p>sfesifhosuhf sdoufhofuhsoudhfodusfd</p>
+        <p>sdffijpsdofdsonfununfoenfunou</p>
+
     </div>
   );
 }
--- src/data/serviceItems.js
+++
@@ -1,8 +0,0 @@
-const services = [
-  { name: "Bricklaying", img: require("../images/services/paint.png") },
-  { name: "Capentry", img: require("../images/services/shinra.png") },
-  { name: "Electrical", img: require("../images/services/shinra.png") },
-  { name: "Waterproofing", img: require("../images/services/paint.png") },
-];
-
-export default services;
